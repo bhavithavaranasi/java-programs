8fa22dc7a1e20e6700de58adf106ee8712db6f21
Bhavithavaranasi <bhavithavaranasi630@gmail.com>
2026-02-24 18:29:43 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class DataTypes
+{
+	public static void main(String args[])
+	{
+		byte b=5;
+		short s=100;
+		int i=16;
+		float f=5.5f;
+		double d=3.1234;
+		long l=1400000;
+		char c='b';
+		boolean b1=true;
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(l);
+		System.out.println(c);
+		System.out.println(b1);
+		
+	}
+}
