787e2d63c96b0fdb674cb66af17d003d08dcdd0f
Bhavithavaranasi <bhavithavaranasi630@gmail.com>
2026-02-25 18:32:04 +0530

    Add files via upload

---
+++ AritheType.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class AritheType
+{
+  public static void main(String args[])
+  {
+    Scanner sc =new Scanner(System.in);
+    System.out.println("enter values");
+    int num1=sc.nextInt();
+    double num2=sc.nextDouble();
+    System.out.println(num1+num2);
+    System.out.println(num1-num2);
+    System.out.println(num1*num2);
+    System.out.println(num1/num2);
+
+
+
+  } 
+}
