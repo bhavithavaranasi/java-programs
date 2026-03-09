1e5e1a93650b216170bbaa7582a866b06dd29110
Varanasi Bhavitha <bhavithavaranasi630@gmail.com>
2026-03-09 18:18:48 +0530

    Add files via upload

---
+++ strings/StringOper.java
@@ -0,0 +1,15 @@
+class StringOper
+{
+ public static void main(String args[])
+ {
+   String str="Bhavi";
+   System.out.println("Length: " +str.length());
+   System.out.println("uppercase: " +str.toUpperCase());
+   System.out.println("lowercase: " +str.toLowerCase());
+   System.out.println("concat: " +str.concat(", Bhavi"));
+   System.out.println("index: " +str.indexOf(3));
+   System.out.println("position: " +str.charAt(2));
+   
+
+  }
+}
