e558421aa0fbda7ef8e888da94a9f6d2d10c0a2c
Varanasi Bhavitha <bhavithavaranasi630@gmail.com>
2026-03-18 19:13:15 +0530

    Add files via upload

---
+++ methods/WithoutParameter.java
@@ -0,0 +1,12 @@
+class WithoutParameter {
+
+    // Method without parameters
+    void greet() {
+        System.out.println("Hello Bhavitha!");
+    }
+
+    public static void main(String[] args) {
+        WithoutParameter obj = new WithoutParameter();
+        obj.greet();  // calling method
+    }
+}
