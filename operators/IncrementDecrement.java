1c81bb5abc57ba2f78d42512226b29a09af0b8b9
Varanasi Bhavitha <bhavithavaranasi630@gmail.com>
2026-03-26 19:04:39 +0530

    Add files via upload

---
+++ operators/IncrementDecrement.java
@@ -0,0 +1,13 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println("Post Increment: " + (a++));
+        System.out.println("After Post Increment: " + a);
+
+        System.out.println("Pre Increment: " + (++a));
+
+        System.out.println("Post Decrement: " + (a--));
+        System.out.println("Pre Decrement: " + (--a));
+    }
+}
